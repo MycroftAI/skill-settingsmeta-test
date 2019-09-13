3d16085df2c1c733dd65dc777684aad8b7d4e2b9
Åke Forslund <ake.forslund@gmail.com>
2019-09-13 15:17:38 +0200

    Add callback

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
 from mycroft import MycroftSkill, intent_file_handler
-from mycroft.skills.audioservice import AudioService
 
 
 class SettingsmetaTest(MycroftSkill):
+    def initialize(self):
+        self.settings.set_changed_callback(self.print_settings)
+
+    def print_settings(self):
+        print(self.settings)
+        self.log.info(self.name)
+        self.speak("Settings changed")
+
     @intent_file_handler('blank.intent')
     def blank(self, message):
         self.speak('No content available')
