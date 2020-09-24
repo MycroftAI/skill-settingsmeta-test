c49193c9a2eff641fe81263f622ccacdc8a9793d
Kris Gesling <kris.gesling@mycroft.ai>
2020-09-25 05:22:30 +0930

    extend settings; add gui display

--- __init__.py
+++ __init__.py
@@ -1,18 +1,32 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class SettingsmetaTest(MycroftSkill):
     def initialize(self):
-        self.settings.set_changed_callback(self.print_settings)
+        self.settings_changed_callback = self.print_settings
+        try:
+            self.gui.register_settings()
+        except AttributeError:
+            self.log.error('GUI settings registration not available')
 
     def print_settings(self):
         print(self.settings)
         self.log.info(self.name)
         self.speak("Settings changed")
 
-    @intent_file_handler('blank.intent')
-    def blank(self, message):
-        self.speak('No content available')
+    @intent_handler('display.settings.intent')
+    def handle_display_settings(self, message):
+        self.speak_dialog('on.screen')
+        self.gui.show_settings()
+
+    @intent_handler('what.is.text.field.intent')
+    def handle_speak_text_field(self, message):
+        if self.settings.get("single_text_field") is None:
+            self.speak_dialog("settings.unavailable")
+        elif self.settings["single_text_field"] == "":
+            self.speak_dialog("setting.field.empty")
+        else:
+            self.speak_dialog(self.settings.single_text_field)
 
 
 def create_skill():
